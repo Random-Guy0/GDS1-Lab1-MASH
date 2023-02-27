9c0979d39e8bba903ddd7a7e63750168f76e5a9f
Amren Gill <amren.gill@gmail.com>
2023-02-28 01:38:17 +1100

    added logic for controlling the helicopter

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Helicopter : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private Vector2 moveAmount;
+    
+    private Camera mainCamera;
+    
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveAmount = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        Vector2 currentPosition = transform.position;
+
+        currentPosition += moveSpeed * Time.deltaTime * moveAmount;
+
+        Vector2 viewportPosition = mainCamera.WorldToViewportPoint(currentPosition);
+
+        if (viewportPosition.x < 0.1f || viewportPosition.x > 0.9f)
+        {
+            currentPosition.x = transform.position.x;
+        }
+
+        if (viewportPosition.y < 0.1f || viewportPosition.y > 0.9f)
+        {
+            currentPosition.y = transform.position.y;
+        }
+
+        transform.position = currentPosition;
+        
+        if (moveAmount.x > 0.0f)
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y = 0.0f;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        else if (moveAmount.x < 0.0f)
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y = 180.0f;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
