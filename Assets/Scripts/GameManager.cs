81cfa02f63b2eaa179b9399e612adf7deeb97fc4
Amren Gill <amren.gill@gmail.com>
2023-02-28 23:59:46 +1100

    added procedural generation of trees and soldiers

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int soldiersOnField;
-    
+    [SerializeField] private Vector2Int minMaxSoldiers;
+    [SerializeField] private Vector2Int minMaxTrees;
+
+    private int soldiersOnField;
+
     [SerializeField] private TMP_Text soldiersInHelicopterText;
     [SerializeField] private TMP_Text soldiersRescuedText;
 
@@ -17,28 +21,21 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject gameOverScreen;
 
     [SerializeField] private GameObject soldierPrefab;
+    [SerializeField] private GameObject treePrefab;
 
     [SerializeField] private AudioSource soldierPickupSound;
+
+    [SerializeField] private float placementDistance;
     
     private int soldiersInHelicopter;
     private int soldiersRescued;
 
     private GameObject[] soldiers;
-    private Vector2[] soldierPositions;
+    private GameObject[] trees;
 
     private void Start()
     {
-        soldiersInHelicopter = 0;
-        soldiersRescued = 0;
-
-        soldiers = GameObject.FindGameObjectsWithTag("Soldier");
-        soldierPositions = new Vector2[soldiers.Length];
-        for (int i = 0; i < soldiers.Length; i++)
-        {
-            soldierPositions[i] = soldiers[i].transform.position;
-        }
-
-        //soldiersOnField = soldiers.Length;
+        ResetGame();
     }
 
     public void PickupSoldier()
@@ -91,27 +88,84 @@ public class GameManager : MonoBehaviour
 
     public void ResetGame()
     {
+        int numSoldiers = Random.Range(minMaxSoldiers.x, minMaxSoldiers.y + 1);
+        int numTrees = Random.Range(minMaxTrees.x, minMaxTrees.y + 1);
+        
+        soldiersOnField = numSoldiers;
         soldiersRescued = 0;
         soldiersInHelicopter = 0;
         UpdateSoldiersInHelicopterText();
         UpdateSoldiersRescuedText();
 
-        soldiers = GameObject.FindGameObjectsWithTag("Soldier");
-        foreach (GameObject soldier in soldiers)
+        if (soldiers != null)
         {
-            DestroyImmediate(soldier);
+            foreach (GameObject soldier in soldiers)
+            {
+                Destroy(soldier);
+            }
         }
-        
-        for (int i = 0; i < soldierPositions.Length; i++)
+
+        if (trees != null)
         {
-            GameObject.Instantiate(soldierPrefab, soldierPositions[i], Quaternion.identity);
+            foreach (GameObject tree in trees)
+            {
+                Destroy(tree);
+            }
         }
+
+        List<Vector2> potentialPoints = GeneratePotentialPoints();
         
-        soldiers = GameObject.FindGameObjectsWithTag("Soldier");
-        soldiersOnField = soldiers.Length;
-        
+        soldiers = new GameObject[numSoldiers];
+        for (int i = 0; i < numSoldiers; i++)
+        {
+            int chosenPointIndex = Random.Range(0, potentialPoints.Count);
+            soldiers[i] = GameObject.Instantiate(soldierPrefab ,potentialPoints[chosenPointIndex], Quaternion.identity);
+            potentialPoints.RemoveAt(chosenPointIndex);
+        }
+
+        trees = new GameObject[numTrees];
+        for (int i = 0; i < numTrees; i++)
+        {
+            int chosenPointIndex = Random.Range(0, potentialPoints.Count);
+            trees[i] = GameObject.Instantiate(treePrefab ,potentialPoints[chosenPointIndex], Quaternion.identity);
+            potentialPoints.RemoveAt(chosenPointIndex);
+        }
+
         winScreen.SetActive(false);
         gameOverScreen.SetActive(false);
         helicopter.StartGame();
+    }
+
+    private List<Vector2> GeneratePotentialPoints()
+    {
+        Camera mainCamera = Camera.main;
+        Vector2 maxPoint = mainCamera.ViewportToWorldPoint(new Vector2(0.9f, 0.9f));
+        Vector2 minPoint = mainCamera.ViewportToWorldPoint(new Vector2(0.5f, 0.1f));
+
+        List <Vector2> potentialPoints = new List<Vector2>();
+
+        Vector2 currentX = minPoint + new Vector2(placementDistance, 0.0f);
+        while (currentX.x < maxPoint.x)
+        {
+            potentialPoints.Add(currentX);
+            currentX.x += placementDistance;
+        }
+        
+        Vector2 currentY = minPoint + new Vector2(0.0f, placementDistance);
+        while (currentY.y < maxPoint.y)
+        {
+            potentialPoints.Add(currentY);
+            currentY.y += placementDistance;
+        }
+        
+        Vector2 currentBoth = minPoint + new Vector2(placementDistance, placementDistance);
+        while (currentBoth.x < maxPoint.x && currentBoth.y < maxPoint.y)
+        {
+            potentialPoints.Add(currentBoth);
+            currentBoth.x += placementDistance;
+            currentBoth.y += placementDistance;
+        }
+        
+        return potentialPoints;
     }
 }
